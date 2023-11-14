28e2e2a5416b5685ec08078086ad5ae2a489a72a
Shashank Kumar <stellarshank.dev@gmail.com>
2023-11-14 23:23:51 +0530

    inint

---
+++ credentials.js
@@ -0,0 +1,6 @@
+
+const CLIENT_ID = "646608371297-ppuq9pphklbcqsje2noamtb5imlsg1v0.apps.googleusercontent.com";
+const CLEINT_SECRET = "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";
+const REDIRECT_URI = "https://developers.google.com/oauthplayground";
+const REFRESH_TOKEN ="1//04ZMY7vQ8xRmPCgYIARAAGAQSNwF-L9Irl7Lobpcs8QqIh_YqRIuVhvgUduBpWEwVJwjxqLFhGTwkavMPkxEZ6SFqsfWrMUvSKVc";
+module.exports = { CLIENT_ID, CLEINT_SECRET, REDIRECT_URI, REFRESH_TOKEN };
